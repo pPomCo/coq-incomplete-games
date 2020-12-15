8cfca695baff29f761ae216019615175e24b503d
pPomCo <pierre.pomeret@irit.fr>
2020-12-15 16:53:38 +0100

    [Drafts] Update Allais/Ellsberg paradoxes

--- DRAFT_allais_ellsberg.v
+++ DRAFT_allais_ellsberg.v
@@ -9,37 +9,41 @@ Local Open Scope ring_scope.
 
 Section Lottery.
 
+  (* TODO: Type for proba values. Must be in [0,1] *) 
   Variable number : realFieldType.
 
-  Inductive lottery outcome :=
-  | ConstantAct (o : outcome) : lottery outcome
-  | MetaLottery (l1 : lottery outcome) (p1 : number) (l2 : lottery outcome) (p2 : number) : lottery outcome.
+
+  Definition outcome := nat.
+
+  (* Lottery = proba on outcomes. *)
+  Inductive lottery :=
+  | ConstantAct (o : outcome) : lottery
+  | MetaLottery (l1 : lottery) (p1 : number) (l2 : lottery) (p2 : number) : lottery.
 
   Notation "'[ o ]" := (ConstantAct o).
   Notation "'[ l1 ~( p1 ); l2 ~( p2 )]" := (MetaLottery l1 p1 l2 p2).
 
 
-  Fixpoint EU Z (u : Z -> number) (l : lottery Z) : number :=
+  Fixpoint EU (u : outcome -> number) (l : lottery) : number :=
     match l with
     | '[o] => u o
     | '[l1~(p1); l2~(p2)] => p1 * EU u l1 + p2 * EU u l2
     end.
   
-
-  Definition preference outcome : rel (lottery outcome). Admitted.
+  Variable preference : rel lottery. 
   Notation " A >> B " := (preference A B) (at level 100).
 
   (*
-  Lemma pref_EU Z u : forall A B : lottery Z, (A >> B) -> EU u A > EU u B.
+  Lemma pref_EU u : forall A B : lottery, (A >> B) -> EU u A > EU u B.
   Admitted.
   *)
   
   (* Si [A(1)] > [B(1)], alors \forall C, \forall p, [A(p);C(1-p)] > [B(p);C(1-p)] *)
-  Definition independence_axiom outcome (A B C : lottery outcome) (p : number) :=
+  Definition independence_axiom (A B C : lottery) (p : number) :=
     (A >> B) -> '[A~(p); C~(1-p)] >> '[B~(p); C~(1-p)].
 
 
-  
+
 
   Section AllaisParadox.
 
@@ -49,19 +53,19 @@ Section Lottery.
     Lemma val_eq : val09 = 1 - val01. Admitted. (* 0.9 = 1 - 0.1 *)
 
     (* Sure to earn nothing *)
-    Definition Z : lottery nat := '[0%N].
+    Definition Z : lottery := '[0%N].
 
     (* Sure tu earn 10000 *)
-    Definition A : lottery nat := '[10000%N].
+    Definition A : lottery := '[10000%N].
 
     (* Earn 15000 with chances 90%, else 0 *)
-    Definition B : lottery nat := '['[15000%N]~(val09); Z~(val01)].
+    Definition B : lottery := '['[15000%N]~(val09); Z~(val01)].
 
     (* Earn 10000 with chances 10%, else 0 *)
-    Definition C : lottery nat := '[A~(val01); Z~(val09)].
+    Definition C : lottery := '[A~(val01); Z~(val09)].
 
     (* Earn 15000 with chances 89%, else 0 *)
-    Definition D : lottery nat := '[B~(val01); Z~(val09)].
+    Definition D : lottery := '[B~(val01); Z~(val09)].
 
     (* Allais's paradox: most people prefer A to B, but D to C *)
     Lemma Allai_indep :
@@ -79,8 +83,8 @@ Section Lottery.
 
     Variables p1 p2 p3 : number.
 
-    Definition won : lottery nat := '[1%N].
-    Definition lost : lottery nat := '[0%N].
+    Definition won : lottery := '[1%N].
+    Definition lost : lottery := '[0%N].
 
     Definition ellsA := '[won~(p1);lost~(p3+p2)].
     Definition ellsB := '[won~(p2);lost~(p3+p1)].
