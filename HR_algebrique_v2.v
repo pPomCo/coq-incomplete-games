42dd340c17406c93d29c364136e8a092ed86a3bb
pPomCo <pierre.pomeret@irit.fr>
2021-01-29 14:20:58 +0100

    [HR] Proof parts

--- HR_algebrique_v2.v
+++ HR_algebrique_v2.v
@@ -5,22 +5,14 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 
-(*
 
-Section EvaluationStructure.
 
-  (* Evaluation structure *)
-  Variables (U W V : Type)
-            (preceq_U : rel U) (preceq_W : rel W) (preceq_V : rel V)
-            (V0 : V)
-            (oplus : Monoid.com_law V0)
-            (otimes : W -> U -> V).
 
-End EvaluationStructure.
 
-*)
 
 Section EvalStruct.
+
+  (* Structure d'évaluation. Contient les opérateurs, les domaines et leurs ordres *)
 
   Record eval_struct : Type :=
     { U : finType ;
@@ -40,26 +32,35 @@ End EvalStruct.
 
 Section Games.
 
-  
-
-  Variable rty : numDomainType.
-
   Section Profiles.
+    (* Profils (de stratégies, de signaux, etc. *)
+
+    (* Domaines *)
     Variables (N : finType)
               (T : N -> finType)
               (X : N -> Type).
 
+
+    (* Profil classique : une action par joueur *)
     Definition profile := {ffun forall i, X i}.
+
+    (* Profil à co-domaine fini *)
     Definition fprofile := {dffun forall i, T i}.
+
+    (* Profil "bayésien" : une action par joueur et par signal *)
     Definition bprofile := {ffun forall it : {i : N & T i}, X (projT1 it)}.
 
+    (* Projection d'un profil "étant donné les signaux" *)
     Definition proj_profile (bp : bprofile) (theta : fprofile) : profile :=
       [ffun i => bp (existT _ i (theta i))].
 
   End Profiles.
 
 
+  
+
   Section SNFGames.
+    (* Jeux classiques en forme normale *)
 
     (* Situation de jeu *)
     Variables (N : finType)
@@ -71,9 +72,14 @@ Section Games.
     Definition SNFutility := u.
 
   End SNFGames.
-    
+
+
+
+
+  
   Section IIGames.
-    
+    (* Jeux à information imparfaite/incomplète *)
+
     (* Situation de jeu *)
     Variables (N : finType)
               (Omega : finType)
@@ -95,6 +101,7 @@ Section Games.
   End IIGames.
 
   Section HGGames.
+    (* Jeux hyper-graphiques *)
     
     (* Situation de jeu *)
     Variables (N : finType)
@@ -107,6 +114,8 @@ Section Games.
               (oplus : forall (i : N), Monoid.com_law (V0 i) )
               (u : forall e i, e \in E -> i \in e -> profile A -> V i).
 
+
+    (* Version "totale" de l'utilité (on donne l'élément neutre si pas d'utilité définie) *)
     Definition u_total e i p :=
       match boolP (e \in E) with
       | AltTrue h1 =>
@@ -116,13 +125,17 @@ Section Games.
         end
       | AltFalse _ => V0 i
       end.
-    
+
+    (* Utilité globale *)
     Definition HGutility (i : N) (p : profile A) : V i :=
       \big[oplus i/V0 i]_(e in [pred e | e \in E]) u_total e i p.
 
   End HGGames.
 
+
+  
   Section SNF_of_HG.
+    (* Tout jeu hyper-graphique correspond à un jeu en forme normale - trivial *)
     
     (* Situation de jeu *)
     Variables (N : finType)
@@ -135,16 +148,17 @@ Section Games.
               (oplus : forall (i : N), Monoid.com_law (V0 i) )
               (u : forall e i, e \in E -> i \in e -> profile A -> V i).
 
-    Check (HGutility _ _ _ _, SNFutility _ _ _).
     Lemma SNF_of_HG :
       HGutility oplus u = SNFutility (HGutility oplus u).
     Proof. by auto. Qed.
+    
   End SNF_of_HG.
 
 
 
   
   Section HR.
+    (* Tout jeu à information imparfaite/incomplète correspond à un jeu hyper-graphique *)
     
     (* Situation de jeu *)
     Variables (N : finType)
@@ -152,24 +166,36 @@ Section Games.
               (Theta : N -> finType)
               (tau : forall (i : N), Omega -> Theta i)
               (A : forall (i : N), finType).
+
     (* Spécification des agents *)
     Variables (E : forall (i : N), eval_struct)
               (d : forall (i : N), Omega -> W (E i))
               (u : forall (i : N), profile A -> Omega -> U (E i)).
 
 
+    (* Ensemble de couples (joueur,signal) correspondant à un profil de signaux *)
     Definition e_theta (theta : fprofile Theta) := [set existT Theta i (theta i) | i in N].
+
+    (* États du monde correspondant à un profil de signaux *)
     Definition omgs_of_theta (theta : fprofile Theta) : {set Omega} := [set omg : Omega | [forall i : N, (tau i omg) == (theta i)]].
     
 
-
+    (* Sommets de l'hyper-graphe = couples (joueur,signal) *)
     Definition bar_N := [finType of {i : N & Theta i}].
+
+    (* Opérateur pour le jeu hyper-graphique *)
     Definition bar_oplus := fun it : bar_N => oplus (E (projT1 it)).
+
+    (* Actions pour le jeu hyper-graphique *)
     Definition bar_A := fun (it : bar_N) => A (projT1 it).
+
+    (* Hyper-arêtes : une pour chaque profil de signaux *)
     Definition bar_E := [set e_theta theta | theta in fprofile Theta].
 
+    (* Profil de signal correspondant à un ensemble e_theta \in bar_E *)
     Definition theta_of_e (e : {set {i : N & Theta i}}) : fprofile Theta. Admitted.
-    
+
+    (* Fonction d'utilité totale pour le jeu hyper-graphique *)
     Definition bar_u_total (e : {set bar_N}) (it : bar_N) bp :=
       let i := projT1 it in
       let t := projT2 it in
@@ -192,236 +218,118 @@ Section Games.
       let theta := theta_of_e e in
       \big[oplus (E i)/V0 (E i)]_(omg in omgs_of_theta theta)
                           otimes (d i omg) (u i (proj_profile bp theta) omg).
-                          
+
+
+
+    (* Morceaux de la preuve *)
+
+
     Lemma eq_oplus (i : N) (t : Theta i) :
       oplus (E i) = bar_oplus (existT _ i t).
     Proof. by auto. Qed.
 
+
+    
     Lemma eq_pred_omg:
       forall i t, [set x | [pred omg | (tau i omg) == t] x] = [set x | (tau i x) == t].
     Proof. by compute. Qed.
 
-    (*
-    Definition forget_first_arg := fun (theta : fprofile Theta) (omg : Omega) => omg.
-    Lemma forget_first_arg_ok :
-      forall theta omg, forget_first_arg theta omg = omg.
-    Proof. by compute. Qed.
-     *)
 
+    
+    Lemma eq_pred_omg2_part i f :
+       \big[oplus (E i)/V0 (E i)]_(theta in fprofile Theta) \big[oplus _/V0 _]_(omg in omgs_of_theta theta) f omg =
+       \big[oplus _/V0 _]_(theta in fprofile Theta) \big[oplus _/V0 _]_(omg <- enum (omgs_of_theta theta)) f omg.
+    Admitted.
+
+    Lemma eq_pred_omg2_part2 i t :
+      enum [set o | tau i o == t] = [seq i2 | i1 <- enum (fprofile Theta), i2 <- enum (omgs_of_theta i1)].
+    Admitted.
+    
     Lemma eq_pred_omg2 :
       forall i t f, \big[oplus (E i)/V0 (E i)]_(omg in [set o | (tau i o) == t]) f omg = \big[oplus _/V0 _]_(theta in fprofile Theta) \big[oplus _/V0 _]_(omg in omgs_of_theta theta) f omg.
     Proof.
     move => i t f.
-    rewrite -!big_enum.
-    Check big_allpairs_dep.
-    Admitted.
+      by rewrite eq_pred_omg2_part -!big_enum -big_allpairs_dep eq_pred_omg2_part2.
+    Qed.
+
+    
 
     Lemma theta_in_bar_E :
-      forall theta, e_theta theta \in bar_E.
+      forall theta, 
+      (@in_mem (Finite.sort (set_of_finType bar_N)) (e_theta theta)
+           (@mem (Finite.sort (set_of_finType bar_N)) (predPredType (Finite.sort (set_of_finType bar_N)))
+              (@SetDef.pred_of_set (set_of_finType bar_N) bar_E))).
     Proof.
     rewrite /bar_E => theta.
     apply/imsetP.
     exists theta => //.
     Qed.
-    
 
-    Check IIutility _ _ _ _ _.
-    Check HGutility _ _ _ _.
-    Check d.
+
+    
+    
+    Lemma theta_of_e_K :
+      cancel e_theta theta_of_e.
+    Admitted.
+
+
+
+    
+    Lemma theta_ffun (theta : fprofile Theta) :
+      theta = [ffun i0 => theta i0].
+    Admitted.
+
+
+
+    Lemma eq_tau_theta theta omg :
+      omg \in omgs_of_theta theta -> [ ffun i0 : N => tau i0 omg] = theta.
+    Proof.
+    rewrite in_set ; move/forallP => Htheta.
+    rewrite (theta_ffun theta) (eq_dffun (tau^~ omg)) => // i.
+    apply/eqP ; rewrite eq_sym.
+    exact : Htheta.
+    Qed.
+
+    Lemma e_theta_inj : {in fprofile Theta &, injective [eta e_theta]}.
+    Proof.
+    rewrite /e_theta => p1 p2 Hp1 Hp2.
+    case (boolP (p1 == p2)); first by move/eqP.
+    move/eqP => Hcontra.
+    Admitted.
+
+
+
+
+    
     Lemma HR :
       forall i t bp, IIutility tau d u t bp = HGutility bar_oplus bar_u (existT _ i t) bp.
     Proof.
     rewrite /IIutility /HGutility.
     move => i t bp.
     rewrite -eq_oplus => //=.
-    rewrite -big_set eq_pred_omg eq_pred_omg2 big_imset.
+    rewrite -big_set eq_pred_omg eq_pred_omg2 big_imset ; last by apply e_theta_inj.
     apply eq_bigr => theta Htheta //=.
     rewrite /u_total.
-    case (boolP (e_theta theta \in bar_E)); last by rewrite {1}theta_in_bar_E.
-    + move => Htheta_barE.
-      rewrite {1}Htheta_barE (* Dependent type error ! *).
+    case (boolP (* e_theta theta \in bar_E *)
+          (@in_mem (Finite.sort (set_of_finType bar_N)) (e_theta theta)
+                   (@mem (Finite.sort (set_of_finType bar_N)) (predPredType (Finite.sort (set_of_finType bar_N)))
+                         (@SetDef.pred_of_set (set_of_finType bar_N) bar_E)))); last by rewrite theta_in_bar_E.
+    - move => Htheta_barE.
+      case (boolP (* existT (fun i1 : N => Theta i1) i t \in e_theta theta *)
+            (@in_mem (Finite.sort bar_N)
+                     (@existT (Finite.sort N) (fun i0 : Finite.sort N => Finite.sort (Theta i0)) i t)
+                     (@mem (Finite.sort bar_N) (predPredType (Finite.sort bar_N))
+                           (@SetDef.pred_of_set bar_N (e_theta theta))))).
+      + move => Hit.
+        rewrite /bar_u theta_of_e_K => /=.
+        apply eq_bigr => omg Homg.
+        rewrite /mk_tprofile => //=.
+          by rewrite (eq_tau_theta Homg).
+      + move => Hcontra.
+          by admit.
     Admitted.
     
   End HR.
 
     
 End Games.
-
-
-
-(*
-
-Section DecisionProblem.
-
-  Record decision_situation : Type :=
-    { Omega : finType ;
-      C : finType ;
-      (* acts : ({ffun Omega -> C})%type ; *)
-    }.
-  
-  Definition act situation : Type := {ffun Omega situation -> C situation}.
-  
-  Record decision_problem : Type := 
-    { situation : decision_situation ;
-      E : eval_struct ;
-      u : C situation -> U E ;
-      Pl : {set (Omega situation)} -> W E ;
-    }.
-
-  Definition ua (D : decision_problem) (a : act (situation D)) : Omega _ -> U _ := fun sow => u (a sow).
-
-  Check ua _.
-
-  Definition ua_inv (D : decision_problem) (a : act (situation D))  : U _ -> {set Omega _} :=
-    fun util => [set sow | (ua a sow) == util].
-
-  Definition range_ua D (a : act (situation D)) : {set U (E D)} :=
-    [set ua a sow | sow in Omega _].
-
-  Definition GEU (D : decision_problem) (a : act (situation D)) : V (E D) :=
-    \big[oplus (E D)/V0 (E D)]_(x in range_ua a) otimes (Pl (ua_inv a x)) x.
-
-  Definition is_dist (X : finType) (Y : Type) X0 (mu : {set X} -> Y) (d : X -> Y) (oplus : Monoid.com_law X0) :=
-    forall A : {set X}, mu A = \big[oplus/X0]_(x in A) d x.
-
-  Check is_dist _ _.
-
-End DecisionProblem.
-
-
-
-Module Game.
-
-  Variable rty : numDomainType.
-
-  Section Profiles.
-    Variables (N : finType)
-              (T : N -> finType)
-              (X : N -> Type).
-
-    Definition profile := {ffun forall i, X i}.
-    Definition fprofile := {dffun forall i, T i}.
-    Definition bprofile := {ffun forall it : {i : N & T i}, X (projT1 it)}.
-
-    Definition proj_profile (bp : bprofile) (theta : fprofile) : profile :=
-      [ffun i => bp (existT _ i (theta i))].
-
-  End Profiles.
-
-
-  Module SNF.
-    Record game : Type :=
-      { N : finType ;
-        A : forall i : N, finType ;
-        u : forall i : N, profile A -> rty ;
-      }.
-  End SNF.
-
-  Module BGame.
-    Record game : Type :=
-      { N : finType ;
-        A : forall i : N, finType ;
-        Theta : forall i : N, finType ;
-        P_cond : profile Theta -> {i : N & Theta i} -> rty ;
-        u : forall i : N, bprofile Theta A -> profile Theta -> rty ;
-      }.
-
-    Definition UE G (i : N G) (t : Theta i) (bp : bprofile (Theta (g:=G)) (A (g:=G))) : rty :=
-      \sum_(theta in [pred p : fprofile (Theta (g:=G)) | p i == t]) P_cond theta (existT _ i (theta i)) * u i bp theta.
-
-  End BGame.
-
-  Module HGGame.
-    Record game : Type :=
-      { N : finType ;
-        E : {set {set N}} ;
-        A : forall i : N, finType ;
-        u : forall e i, e \in E -> i \in e -> profile A -> rty ; 
-      }.
-
-    Search _ (is_true (_ && _)).
-    
-    Definition u_total G e i :=
-      match boolP (e \in E G) with
-      | AltTrue h1 =>
-        match boolP (i \in e) with
-        | AltTrue h2 => u h1 h2
-        | AltFalse _ => fun _ => 0%R
-        end
-      | AltFalse _ => fun _ => 0%R
-      end.
-    
-    Definition UG G i p : rty :=
-      \sum_(e in E G) u_total e i p.
-
-  End HGGame.
-
-  Definition SNFGame_2_HGGame (G : SNF.game) : HGGame.game :=
-    {| HGGame.N := SNF.N G ;
-       HGGame.E := set1 setT ;
-       HGGame.A := SNF.A (g:=G) ;
-       HGGame.u := fun _ i _  _ => SNF.u i
-    |}.
-
-  Definition HGGame_2_SNFGame (G : HGGame.game) : SNF.game :=
-    {| SNF.N := HGGame.N G ;
-       SNF.A := HGGame.A (g:=G) ;
-       SNF.u := HGGame.UG  (G:=G) ;
-    |}.
-      
-  Definition SNFGame_2_BGame (G : SNF.game) : BGame.game :=
-    {| BGame.N := SNF.N G ;
-       BGame.A := SNF.A (g:=G) ;
-       BGame.Theta := fun _ => unit_finType ;
-       BGame.P_cond := fun _ _ => 1%R ;
-       BGame.u := fun i bp _ => SNF.u i (proj_profile bp [ffun _ => tt]) ;
-    |}.
-
-  Search _ "imset".
-  Check (_ : bprofile _ _) _.
-  Definition BGame_2_HGGame (G : BGame.game) : HGGame.game. Admitted.
-                                               (*:= 
-    {| HGGame.N := [finType of {i : BGame.N G & BGame.Theta i}] ;
-       HGGame.E := [set [set (existT _ i (theta i)) | i in BGame.N G] | theta in bprofile BGame.Theta _ _] ;
-    |}. *)
-
-
-  Check BGame.UE _.
-  Check HGGame.UG _.
-  Lemma lemma1 :
-    forall (G : BGame.game) (i : BGame.N G) (t : BGame.Theta i) (bp : bprofile _ _),
-    BGame.UE t bp = HGGame.UG (existT _ i t) bp.
-  
-End Game.
-
-Module GamesM.
-  Definition X := 1.
-End GamesM.  
-
-Check GamesM.SNF.u _ _.
-
-Definition evaluation_structure.
-Definition decision_problem.
-Definition GEU.
-Definition dist.
-Definition SNF_game.
-Definition profile.
-Definition HG_game.
-Definition global_utility.
-Definition BGame.
-Definition PiGame.
-Definition bprofile.
-Definition UE.
-Definition eqNash.
-Definition eqBay.
-Definition UUGame.
-Lemma is_decision_problem.
-Lemma is_SNFGame
-Lemma is_BGame.
-Lemma is_PiGame.
-
-Definition UUGame2HGGame.
-Lemma HowsonRonsenthal.
-Definition HGGame2SNFGame.
-Definition UUGame2SNFGame.
- *)
